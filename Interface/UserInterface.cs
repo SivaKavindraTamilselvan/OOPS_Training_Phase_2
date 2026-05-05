11438484746e986c72b5f75c147e8cee25910f97
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-05-05 16:49:18 +0530

    updation made in user interface

--- Interface/UserInterface.cs
+++ Interface/UserInterface.cs
@@ -4,9 +4,12 @@ namespace NotificationApp.Interfaces;
 internal interface IUserService
 {
     public User AddUser();
+    public void PrintAllUsers();
+    public User? GetUserById(int id);
     public User? GetUserByEmail(string email);
     public User? GetUserByPhoneNumber(string phonenumber);
-    public void PrintAllUsers();
     public User? DeleteUserByPhoneNumber(string phonenumber);
     public User? DeleteUserByEmail(string email);
+    public User? UpdateUserById(int userid);
+
 }
